e92d943259871d15d511e4b9ba464e2d5f615893
Hero <76834176+jish0101@users.noreply.github.com>
2023-02-01 14:37:09 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,204 @@
-import Navbar from "./components/navbar/Navbar";
-import {
-  Header,
-  Features,
-  Download,
-  Faq,
-  Cta,
-  Footer,
-} from "./containers/index";
-import "./index.css";
+import { useReducer, useRef } from "react";
+import TodoList from "./components/TodoList";
+import useLocalStorage from "./hooks/useLocalStorage";
+import { v4 as uuidv4 } from "uuid";
+import sunIcon from "../images/icon-sun.svg";
+import moonIcon from "../images/icon-moon.svg";
 
 function App() {
+  const [todos, setTodos] = useLocalStorage("todos", []);
+  const [active, setActive] = useReducer(reducer, {
+    all: true,
+    left: false,
+    complete: false,
+  });
+  const todoRef = useRef();
+  let todosLeft = todos.filter((todo) => !todo.complete);
+  let todosComplete = todos.filter((todo) => {
+    if (todo.complete) {
+      return todo;
+    }
+  });
+
+  const [darkTheme, setDarkTheme] = useLocalStorage("theme", false);
+
+  function addTodo(e) {
+    e.preventDefault();
+    const todoText = todoRef.current.value;
+    if (!todoText) return;
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        { id: uuidv4(), todoText: todoText, complete: false },
+      ];
+    });
+    todoRef.current.value = "";
+  }
+
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = todos.find((todo) => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
+
+  function deleteTodo(id) {
+    if (id) {
+      const newTodos = todos.filter((todo) => todo.id !== id);
+      setTodos(newTodos);
+    } else {
+      setTodos(todosLeft);
+    }
+  }
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "all":
+        return {
+          all: true,
+          left: false,
+          complete: false,
+        };
+      case "left":
+        return {
+          all: false,
+          left: true,
+          complete: false,
+        };
+      case "complete":
+        return {
+          all: false,
+          left: false,
+          complete: true,
+        };
+      default:
+        return state;
+    }
+  }
+
   return (
-    <div className="App">
-      <div className="container">
-        <Navbar />
-        <Header />
-        <Features />
-        <Download />
-        <Faq />
+    <div className="App" data-theme={`${darkTheme ? "dark" : "light"}`}>
+      <div className="todo__container">
+        <div className="todo__header">
+          <div className="todo__header_container">
+            <h1 className="todo__header_container_primary-heading">T O D O</h1>
+            <button
+              className="todo__theme-toggle"
+              onClick={() => setDarkTheme((value) => !value)}
+            >
+              <img
+                src={`${darkTheme ? sunIcon : moonIcon}`}
+                alt="theme-toggle"
+              />
+            </button>
+          </div>
+          <div className="todo__header_input-container">
+            <form onSubmit={(e) => addTodo(e)}>
+              <button className="todo__header_input-btn">&#43;</button>
+              <input
+                type="text"
+                placeholder="Create a new todo..."
+                ref={todoRef}
+              />
+            </form>
+          </div>
+        </div>
+        <div className="todo__todo-list_container">
+          {active.all && (
+            <TodoList
+              todos={todos}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+              setTodos={setTodos}
+            />
+          )}
+          {active.left && (
+            <TodoList
+              todos={todosLeft}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+              setTodos={setTodos}
+            />
+          )}
+          {active.complete && (
+            <TodoList
+              todos={todosComplete}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+              setTodos={setTodos}
+            />
+          )}
+          <div className="todo__footer">
+            <div className="todo__footer_todos-left">
+              <span>
+                {todosLeft.length +
+                  `${todosLeft.length > 1 ? " items " : " item "} left`}
+              </span>
+            </div>
+            <div className="todo__footer_filters">
+              <button
+                onClick={() => setActive({ type: "all" })}
+                className={`todo__filter-btn ${
+                  active.all ? "task-filter-active" : null
+                }`}
+              >
+                All
+              </button>
+              <button
+                onClick={() => setActive({ type: "left" })}
+                className={`todo__filter-btn ${
+                  active.left ? "task-filter-active" : null
+                }`}
+              >
+                Active
+              </button>
+              <button
+                onClick={() => setActive({ type: "complete" })}
+                className={`todo__filter-btn ${
+                  active.complete ? "task-filter-active" : null
+                }`}
+              >
+                Completed
+              </button>
+            </div>
+            <div className="todo__footer-clear">
+              <button
+                className="todo__footer-clear-btn todo__filter-btn"
+                onClick={() => deleteTodo()}
+              >
+                Clear Completed
+              </button>
+            </div>
+          </div>
+          <div className="todo__footer_filters-sm-only">
+            <button
+              onClick={() => setActive({ type: "all" })}
+              className={`todo__filter-btn ${
+                active.all ? "task-filter-active" : null
+              }`}
+            >
+              All
+            </button>
+            <button
+              onClick={() => setActive({ type: "left" })}
+              className={`todo__filter-btn ${
+                active.left ? "task-filter-active" : null
+              }`}
+            >
+              Active
+            </button>
+            <button
+              onClick={() => setActive({ type: "complete" })}
+              className={`todo__filter-btn ${
+                active.complete ? "task-filter-active" : null
+              }`}
+            >
+              Completed
+            </button>
+          </div>
+        </div>
+        <p id="drag-n-drop">Drag and drop to reorder list</p>
       </div>
-      <Cta />
-      <Footer />
     </div>
   );
 }
--- style-guide.md
+++ style-guide.md
@@ -11,13 +11,28 @@ The designs were created to the following widths:
 
 ### Primary
 
-- Soft Blue: hsl(231, 69%, 60%)
-- Soft Red: hsl(0, 94%, 66%)
+- Bright Blue: hsl(220, 98%, 61%)
+- Check Background: linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)
 
 ### Neutral
 
-- Grayish Blue: hsl(229, 8%, 60%)
-- Very Dark Blue: hsl(229, 31%, 21%)
+### Light Theme
+
+- Very Light Gray: hsl(0, 0%, 98%)
+- Very Light Grayish Blue: hsl(236, 33%, 92%)
+- Light Grayish Blue: hsl(233, 11%, 84%)
+- Dark Grayish Blue: hsl(236, 9%, 61%)
+- Very Dark Grayish Blue: hsl(235, 19%, 35%)
+
+### Dark Theme
+
+- Very Dark Blue: hsl(235, 21%, 11%)
+- Very Dark Desaturated Blue: hsl(235, 24%, 19%)
+- Light Grayish Blue: hsl(234, 39%, 85%)
+- Light Grayish Blue (hover): hsl(236, 33%, 92%)
+- Dark Grayish Blue: hsl(234, 11%, 52%)
+- Very Dark Grayish Blue: hsl(233, 14%, 35%)
+- Very Dark Grayish Blue: hsl(237, 14%, 26%)
 
 ## Typography
 
@@ -25,17 +40,7 @@ The designs were created to the following widths:
 
 - Font size: 18px
 
-### Fonts
+### Font
 
-- Family: [Rubik](https://fonts.google.com/specimen/Rubik)
-- Weights: 400, 500
-
-## Icons
-
-For the icons in the design, you can choose between using the icons provided or an font icon library.
-
-Some library suggestions can be found below:
-
-- [Font Awesome](https://fontawesome.com)
-- [IcoMoon](https://icomoon.io)
-- [Ionicons](https://ionicons.com)
+- Family: [Josefin Sans](https://fonts.google.com/specimen/Josefin+Sans)
+- Weights: 400, 700
